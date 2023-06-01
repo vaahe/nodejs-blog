7f2b05d5b33f8d71d55240607edcbb6ca305b1bc
vaahe <vahebarseghyan9999@gmail.com>
2023-06-01 15:37:28 +0400

    add pagination

---
+++ frontend/src/components/Pagination.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { usePagination, DOTS } from '../hooks/usePagination';
+
+export const Pagination = (props) => {
+    const { onPageChange, totalCount, siblingCount = 1, currentPage, pageSize } = props;
+
+    const paginationRange = usePagination({
+        currentPage,
+        totalCount,
+        siblingCount,
+        pageSize
+    });
+
+    if (currentPage === 0 || paginationRange.length < 2) {
+        return null;
+    }
+
+    const onNext = () => {
+        onPageChange(currentPage + 1);
+    };
+
+    const onPrevious = () => {
+        onPageChange(currentPage - 1);
+    };
+
+    let lastPage = paginationRange[paginationRange.length - 1];
+    let firstPage = paginationRange[0];
+    console.log(firstPage, lastPage, currentPage);
+
+    return (
+        <nav className="flex items-center justify-center w-full border border-black mb-24 p-2">
+            <ul className="inline-flex items-center -space-x-px">
+                <li>
+                    <button className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={onPrevious} disabled={currentPage === firstPage}>
+                        <span className="sr-only">Previous</span>
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                    </button>
+                </li>
+
+                {paginationRange && paginationRange.map(pageNumber => {
+                    if (pageNumber === DOTS) {
+                        return <li>&#8230;</li>;
+                    }
+
+                    return (
+                        <li
+                            key={pageNumber}
+                            className="px-3 py-2 cursor-pointer leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                            onClick={() => onPageChange(pageNumber)}>
+                            {pageNumber}
+                        </li>
+                    );
+                })}
+
+                <li>
+                    <button className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" onClick={onNext} disabled={currentPage === lastPage}>
+                        <span className="sr-only">Next</span>
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
